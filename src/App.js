c513165f46e367d99f5b2e08cb241e7d7c463208
Gerard Eklu <gerard_eklu@homedepot.com>
2019-07-28 20:42:34 -0400

    Implemented shop page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/homepage";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 
 const App = () => {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
