bf422cc96ba2b575c13d1d2960821be700a1b227
Gerard Eklu <gerard_eklu@homedepot.com>
2019-08-04 12:23:48 -0400

    Implemented the sign in and form input component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/Header";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in";
+import "./SignInAndSignUpPage.scss";
+
+const SignInAndSignupPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignupPage;
