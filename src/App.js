959e7abba0a5c803b145187dfa2e56564468c0cc
Gerard Eklu <gerard_eklu@homedepot.com>
2019-09-09 22:15:05 -0400

    Integrated firbase into app, sign in and sign out for users

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
     return (
       <div>
         <div className="App">
-          <Header />
+          <Header currentUser={this.state.currentUser} />
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/shop" component={ShopPage} />
