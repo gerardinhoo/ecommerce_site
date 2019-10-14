2ea10efd96f9b947ea2d2abc3308b921b0a1a41e
Gerard Eklu <gerard_eklu@homedepot.com>
2019-10-13 21:01:03 -0400

    Stored user data in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
   appId: "1:334393019524:web:ff84ab6e56b705b5"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+    return userRef;
+  }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
